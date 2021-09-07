58e1fb65df713acc170e909cc1e4e5d8cb478b5d
wangyz <wangyuzhong@cmdi.chinamobile.com>
2021-09-07 11:46:35 +0800

    首页代码提交

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -12,7 +12,7 @@ import logo from '../../assets/images/logo.png'
 import { reqLogin } from '../../api'
 import memoryUtils from '../../utils/memoryUtils'
 import storageUtils from '../../utils/storageUtils'
-import { Redirect } from 'react-router-dom'
+// import { Redirect } from 'react-router-dom'
 
 export default class Login extends Component {
 
@@ -63,16 +63,15 @@ export default class Login extends Component {
       } else {
         callback() // 验证通过
       }
-    }
-    
+  }
 
   render() {
       //如果用户已经登录则跳转到管理页面
-    const user = memoryUtils.user
-    if (user || user._id) {
-      //自动跳转到登录
-      return <Redirect to='/'/>;
-    }
+    // const user = memoryUtils.user
+    // if (user || user._id) {
+    //   //自动跳转到登录
+    //   return <Redirect to='/'/>;
+    // }
     return (
       <div className="login">
         <header className="login-header">
